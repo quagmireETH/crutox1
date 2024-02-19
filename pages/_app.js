678b77306dd50abe4a36ac0cf1184a86cc8a048f
Digi | Toxic <DigiToxic55@gmail.com>
2024-02-19 14:48:16 -0500

    official

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,30 @@
 import "../styles/globals.css";
 import CustomCursor from "./components/CustomCursor";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="relative">
-      <CustomCursor />
-      {Array.from({ length: 1000 }).map((_, idx) => (
-        <div
-          key={idx}
-          className="star absolute" // Make sure to have absolute positioning in your CSS for 'star'
-          style={{
-            top: `${Math.random() * 100}%`, // Use percentage for full height coverage
-            left: `${Math.random() * 100}%`, // Use percentage for full width coverage
-            animationDuration: `${0.5 + Math.random() * 2}s`, // random shine duration
-            animationDelay: `${Math.random() * 1.5}s`, // random shine delay
-          }}
-        />
-      ))}
-      <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Crutox</title>
+      </Head>
+      <div className="relative">
+        <CustomCursor />
+        {Array.from({ length: 1000 }).map((_, idx) => (
+          <div
+            key={idx}
+            className="star absolute" // Make sure to have absolute positioning in your CSS for 'star'
+            style={{
+              top: `${Math.random() * 100}%`, // Use percentage for full height coverage
+              left: `${Math.random() * 100}%`, // Use percentage for full width coverage
+              animationDuration: `${0.5 + Math.random() * 2}s`, // random shine duration
+              animationDelay: `${Math.random() * 1.5}s`, // random shine delay
+            }}
+          />
+        ))}
+        <Component {...pageProps} />
       </div>
+    </>
   );
 }
 
